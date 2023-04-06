018672ed05e89b960266ac2cd7b41e416e8e32d2
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2023-04-06 15:20:23 +0000

    font problem fixed

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import '../styles/globals.css'
 import '@splidejs/react-splide/css';
 import Navbar from '../components/Navbar'
 import { Barlow, Barlow_Condensed, Bellefair } from 'next/font/google'
-const barlow = Barlow({ weight: "400", subsets: ['latin'], variable: '--font-barlow', })
+const barlow = Barlow({ weight: "400", subsets: ['latin'], variable: '--font-barlow', display: 'swap' })
 
-const barlowCondensed = Barlow_Condensed({ weight: "400", subsets: ['latin'], variable: '--font-barlowCondensed', })
+const barlowCondensed = Barlow_Condensed({ weight: "400", subsets: ['latin'], variable: '--font-barlowCondensed', display: 'swap' })
 
-const bellefair = Bellefair({ weight: "400", subsets: ['latin'], variable: '--font-bellefair', })
+const bellefair = Bellefair({ weight: "400", subsets: ['latin'], variable: '--font-bellefair', display: 'swap' })
 function MyApp({ Component, pageProps }) {
   return <>
     <div className={`${barlow.variable} ${bellefair.variable} ${barlowCondensed.variable}`}>
